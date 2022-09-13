2ee779558bf81a1a3a4d8ddfb22426df6a97c712
Shakir Ali <031-19-0009@sibadomain.local>
2022-09-13 16:51:22 +0500

    done with the UI of the StopWatch App

--- StopWatchExample/app/src/main/java/com/example/stopwatchexample/MainActivity.java
+++ StopWatchExample/app/src/main/java/com/example/stopwatchexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.stopwatchexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            savedInstanceState.getInt()
+        }
+    }
+
+    public void onClickStart(View view) {
+        TextView timerText = findViewById(R.id.textTime);
+
     }
 }
